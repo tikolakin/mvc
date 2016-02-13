105f43ddb9a8612a9ad4248d50bfd49d0617509a
Tiko Lakin <tiko@tiko.local.drupalcompany.com>
2016-02-13 13:22:42 +0200

    initialing the project

---
+++ app/Core/Route.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tiko
+ * Date: 13.02.16
+ * Time: 11:06
+ */
+
+namespace Acme\Core;
+
+
+class Route
+{
+    public static function start() {
+        echo "Hello World";
+    }
+}
---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tiko
+ * Date: 13.02.16
+ * Time: 10:54
+ */
+
+require_once 'vendor/autoload.php';
+
+use Acme\Core\Route;
+use Acme\Core\Model;
+use Acme\Core\View;
+use Acme\Core\Controller;
+
+Route::start();
