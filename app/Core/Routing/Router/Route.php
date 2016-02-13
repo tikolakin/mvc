7b490b4a1e51816233cfdcfdb5f66a4cdbbc740a
Tiko Lakin <tiko@tiko.local.drupalcompany.com>
2016-02-13 14:22:31 +0200

    added routes

--- app/Core/Routing/Router/Route.php
+++ app/Core/Routing/Router/Route.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: tiko
  * Date: 13.02.16
- * Time: 13:19
+ * Time: 13:35
  */
 
-namespace Acme\Core;
+namespace Acme\Core\Routing\Route;
 
 
-class Controller
+class Route
 {
 
 }
